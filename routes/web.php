a5fe0515cfd477b8546eaffc0612b2f1fbf25bc0
RizqyNurfauzella <fauzella171@gmail.com>
2025-03-15 02:15:35 +0700

    Update web.php : "Memisahkan halaman public, dashboard user login & verified,, grouping page login-required"

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,32 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Halaman Public
+Route::get('/', fn () => view('welcome'));
+Route::get('/about', fn () => view('about'));
+Route::get('/review', fn () => view('review'));
+Route::get('/faq', fn () => view('faq'));
+Route::get('/contact', fn () => view('contact'));
+Route::get('/explore', fn () => view('explore'));
 
-Route::middleware('auth')->group(function () {
+// Dashboard untuk user login & verified
+Route::get('/dashboard', fn () => view('dashboard'))
+    ->middleware(['auth', 'verified'])->name('dashboard');
+
+// Group untuk halaman login-required
+Route::middleware(['auth'])->group(function () {
+    Route::view('/tour', 'tour');
+    Route::view('/cart', 'cart');
+    Route::view('/settings', 'settings');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+// Route auth (login/register/logout)
 require __DIR__.'/auth.php';

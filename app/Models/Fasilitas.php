2424e40586afead3159bbf061ae712a3b70f6109
Muhammad Ihsan Firjatulloh Al Khasaf <muhihsanfa@gmail.com>
2025-06-20 11:08:54 +0700

    update reservasi

--- app/Models/Fasilitas.php
+++ app/Models/Fasilitas.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Destinations extends Model
+class Fasilitas extends Model
 {
     /** @use HasFactory<\Database\Factories\DestinationsFactory> */
     use HasFactory;

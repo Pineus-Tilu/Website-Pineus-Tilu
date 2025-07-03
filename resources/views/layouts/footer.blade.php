f870fd2b0a42eb077c93204a0cf17d07d14c0e42
Muhammad Ihsan Firjatulloh Al Khasaf <muhihsanfa@gmail.com>
2025-07-03 22:11:03 +0700

    footer update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -14,6 +14,7 @@
       <div class="space-y-2">
         <h4 class="font-semibold">Pertanyaan Umum</h4>
         <ul class="space-y-1">
+          <li><a href="/tentang" class="hover:underline">Peraturan dan Ketentuan</a></li>
           <li><a href="/tentang" class="hover:underline">Penjadwalan Ulang</a></li>
           <li><a href="/tentang" class="hover:underline">Pembatalan</a></li>
         </ul>
@@ -24,11 +25,11 @@
         <h4 class="font-bold">Contact</h4>
         <div class="flex items-center space-x-2">
           <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp" class="w-5 h-5">
-          <a href="https://wa.me/6287735482327" target="_blank" class="hover:underline">+62 877-3548-2327</a>
+          <a href="https://wa.me/6287735482327" target="_blank" class="hover:underline">Admin 1</a>
         </div>
         <div class="flex items-center space-x-2">
           <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp" class="w-5 h-5">
-          <a href="https://wa.me/6281220413424" target="_blank" class="hover:underline">+62 812-2041-3424</a>
+          <a href="https://wa.me/6281220413424" target="_blank" class="hover:underline">Admin 2</a>
         </div>
         <div class="flex items-center space-x-2">
           <img src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/gmail.ico" alt="Gmail" class="w-4 h-4">

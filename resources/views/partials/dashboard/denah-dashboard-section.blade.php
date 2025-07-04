60a783c8456649d5d739be1fb38cd2d947ce18b7
RizqyNurfauzella <fauzella171@gmail.com>
2025-07-04 23:10:18 +0700

    Update UI Dashboard Section

--- resources/views/partials/dashboard/denah-dashboard-section.blade.php
+++ resources/views/partials/dashboard/denah-dashboard-section.blade.php
@@ -39,13 +39,13 @@
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0zM10 7v3m0 0v3m0-3h3m-3 0H7"></path>
                                     </svg>
                                 </div>
-                                <p class="text-white font-bold text-base md:text-lg font-typewriter">Klik untuk Zoom</p>
+                                <p class="text-white font-bold text-base md:text-lg font-typewriter">Klik untuk Melihat</p>
                             </div>
                         </div>
 
                         <!-- Corner Badge -->
                         <div class="absolute top-2 right-2 md:top-4 md:right-4 bg-green-500 text-white px-3 py-1 md:px-4 md:py-2 rounded-full text-xs md:text-sm font-bold shadow-lg">
-                            📍 Layout Site
+                            📍 Klik untuk Melihat
                         </div>
                     </div>
 
